15be470193a80bbfa0d33bbbd120a437b411792b
Underrated_programmer <111236294+underrated-X@users.noreply.github.com>
2025-02-23 20:12:12 +0100

    Add files via upload

---
+++ favourite colour/fav_colour.js
@@ -0,0 +1,63 @@
+ const favourite = 'purple';
+ const colour = prompt ( "What's your favourite colour? ");
+ if ( colour === 'blue' ||  colour === 'sky blue' ||  colour === 'navy blue' ||  colour === 'dark blue' ||  colour === 'BLUE' || colour ===  'baby blue'){
+    document.write( " Blue is a soft colour! 🪂");
+ } else if ( colour === 'yellow' ||  colour === 'YELLOW' ||  colour === 'light yellow' || colour ===  'dark yellow'){
+    document.write( " Yellow is a warm colour!⭐");
+ }else if ( colour === 'red' ||  colour === 'burgundy' ||  colour === 'wine' ||  colour === 'RED'){
+   document.write( " Red says Romance 💓");
+}else if ( colour === 'brown' ||  colour === 'BROWN' ||  colour === 'light brown' ||  colour === 'dark brown'){
+   document.write( " Brown smells like chocolate 😋");
+} else if ( colour === 'green'||  colour === 'olive green' ||  colour === 'dark green' ||  colour === ' light green' ||  colour === 'GREEN'){
+   document.write( " Green is an Apple or maybe a Pear😁");
+}else if ( colour === 'pink' ||  colour === 'baby pink' ||  colour === 'light pink' ||  colour === 'dark pink' ||  colour === 'PINK'){
+   document.write( " Pink screams beauty 🌸");
+}else if ( colour === 'black' ||  colour === 'BLACK'){
+   document.write( " Black screams power! 🖤");
+}else if ( colour === 'white' ||  colour === 'WHITE'){
+   document.write( " White is peaceful 🌼");
+}else if ( colour === 'purple' ||  colour === 'lavender' || colour === 'PURPLE' || colour ===  'lilac' ||  colour === 'onion purple'){
+   document.write( " Purple is Alluring 💐");
+}else if ( colour === 'lemon' ||  colour === 'LEMON'){
+   document.write( " lemon as in Lemonade 😏");
+} else if ( colour === 'lime' ||  colour === 'LIME'){
+   document.write( "Urghhhh!  Bitter 🤢");
+}else if ( colour === 'coffee' ||   colour === 'COFFEE'){
+   document.write( " Espresso Please 🥺");
+}else if ( colour === 'gold' ||  colour === 'GOLD'){
+   document.write( "As in gold-digger 😂");
+}else if ( colour === 'silver' ||  colour === 'SILVER'){
+   document.write( " Is that also a Colour 🤔");
+}else if ( colour === 'orange' ||   colour === 'light orange' ||  colour === 'dark orange' ||  colour === 'ORANGE'){
+   document.write( " That's more of a Fruit! 🍊");
+}else if ( colour === 'chocolate' || colour ===  'dark chocolate' ||  colour === 'CHOCOLATE'){
+   document.write( "Dark and hot are the best 🤗 ");
+}else if ( colour === 'plum' ||  colour === 'PLUM'){
+   document.write( "That's more of a flower🌷 ");
+}else if ( colour === 'neon' || colour ===  'NEON'){
+   document.write( " That's more of an aesthetic 🤩 ");
+}else if ( colour === 'peach'||  colour === 'PEACH'){
+   document.write( " Ripe ones are definitely the best😋 ");
+}else if ( colour === 'rainbow' ||  colour === 'RAINBOW'){
+   document.write( " Seven unique shades of colour🌈 ");
+}else if ( colour === 'caramel' ||  colour === 'CARAMEL'){
+   document.write( " It's one of a kind 😇 ");
+}
+else if ( colour === ''){
+    document.write( " please input a colour");
+ }else {
+    document.write( " What a great taste of colour you have!🤭");
+ }
+//  if ( colour !== 'purple') {
+//     document.write( " But why not purple ):");
+// } else{
+// }
+// if ( colour === '') {
+//    document.write(!== " But why not purple ):");
+// }
+if ( colour !== 'purple') {
+   console.log( " But why not purple ):")
+}
+// if ( colour !== 'purple') {
+//  alert( " Why not purple ):")
+// }
